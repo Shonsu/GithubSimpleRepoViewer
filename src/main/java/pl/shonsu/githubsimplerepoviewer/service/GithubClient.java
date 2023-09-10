75a29073f945c4b4c91c26241264226466391cec
Shonsu <shonsu82@gmail.com>
2023-09-10 14:18:11 +0200

    trying with Parallel calls to external api

--- src/main/java/pl/shonsu/githubsimplerepoviewer/service/GithubClient.java
+++ src/main/java/pl/shonsu/githubsimplerepoviewer/service/GithubClient.java
@@ -11,6 +11,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import pl.shonsu.githubsimplerepoviewer.exception.GithubNotFoundException;
 import pl.shonsu.githubsimplerepoviewer.model.GithubBranch;
 import pl.shonsu.githubsimplerepoviewer.model.GithubRepository;
+import pl.shonsu.githubsimplerepoviewer.model.GithubSimpleRepository;
+import pl.shonsu.githubsimplerepoviewer.model.SimpleBranch;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.HashSet;
@@ -52,6 +55,37 @@ public class GithubClient {
                 .flatMap(this::validate)
                 .block();
     }
+
+    public Mono<List<GithubBranch>> getUserRepositoryBranchesMono(String owner, String repository) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/repos/{owner}/{repository}/branches")
+                        .build(owner, repository))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<GithubBranch>>() {
+                })
+                .flatMap(this::validate);
+    }
+
+    public List<GithubBranch> getUserRepositoryBranchesInParallel(String owner, List<GithubRepository> repositories) {
+        return Flux.fromIterable(repositories)
+                .flatMap(repo -> getUserRepositoryBranchesMono(owner, repo.name()).zipWith(repo, branches ->
+                                new GithubSimpleRepository(repo.name(), repo.ownerLogin(), branches.stream().map(githubBranch -> new SimpleBranch(
+                                                githubBranch.name(),
+                                                githubBranch.sha()
+                                        )).toList()
+                                ))
+                )
+//
+//                .toStream()
+//                .flatMap(List::stream)
+//                .toList();
+    }
+
+
+//    private GithubSimpleRepository createSimpleRepo(String repoName,String repoOwner, Mono<List<GithubBranch>> monoBranches){
+//        return monoBranches.share().block(githubBranches -> new GithubSimpleRepository(repoName, repoOwner, githubBranches.stream().map(githubBranch -> new SimpleBranch(githubBranch.name(), githubBranch.sha())).toList()));
+//    }
+
 
     private <T> Mono<List<T>> validate(List<T> items) {
         Set<ConstraintViolation<T>> violations = new HashSet<>();
